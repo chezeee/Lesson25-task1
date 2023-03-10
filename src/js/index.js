1323cc1b16117ad17f335ade7200903433029795
Ilya Yalymov <chezeee@gmail.com>
2023-03-10 22:03:26 +0300

    Solution added

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,7 @@
+const currencyCards = document.querySelectorAll("button");
 
+currencyCards.forEach((card) => {
+  card.addEventListener("click", (event) => {
+    event.currentTarget.classList.toggle("active");
+  });
+});
