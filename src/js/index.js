50077cb837f26b866f64d9a36f22af3bebf4d36b
Ilya Yalymov <chezeee@gmail.com>
2023-03-12 15:58:12 +0300

    Initial structure added

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,12 @@
-const currencyCards = document.querySelectorAll("button");
+// TODO: implement FetchWrapper
 
-currencyCards.forEach((card) => {
-  card.addEventListener("click", (event) => {
-    event.currentTarget.classList.toggle("active");
+// Sample usage - do not modify
+try {
+  const API = new FetchWrapper(
+    "https://jsdemo-3f387-default-rtdb.europe-west1.firebasedatabase.app"
+  );
+
+  API.get("/notifications/new.json").then((data) => {
+    console.log(data);
   });
-});
+} catch (error) {}
